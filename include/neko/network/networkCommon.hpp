a21f84d6ad19c4ae6c6edda623636b8bf3b1f307
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-12-15 02:51:19 +0900

    Update project version to 1.0.3 in CMakeLists.txt and networkCommon.hpp

--- include/neko/network/networkCommon.hpp
+++ include/neko/network/networkCommon.hpp
@@ -353,7 +353,7 @@ namespace neko::network {
         } else {
             config::globalConfig
                 .setProtocol("https://")
-                .setUserAgent("NekoNetwork/v1.0.2 +https://github.com/moehoshio/NekoNetwork")
+                .setUserAgent("NekoNetwork/v1.0.3 +https://github.com/moehoshio/NekoNetwork")
                 .setProxy("true");
         }
     }
